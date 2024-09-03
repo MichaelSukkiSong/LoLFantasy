a7f2bbee053044c51134e3a12117dc9f5136e193
MichaelSukkiSong <sa77irak@gmail.com>
2024-09-03 20:00:46 +0900

    add: helper config

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+abstract contract CodeConstants {
+    // Chain IDs
+    uint256 public constant SEPOLIA_CHAIN_ID = 11133111;
+    uint256 public constant ANVIL_CHAIN_ID = 31337;
+
+    // VRF mock constructor variables
+    uint96 public constant BASE_FEE = 1e18;
+    uint96 public constant GAS_PRICE = 2e10;
+    int256 public constant WEI_PER_UNIT_LINK = 1e18;
+}
+
+contract HelperConfig is CodeConstants, Script {
+    struct NetworkConfig {
+        address vrfCoordinator;
+        bytes32 keyHash;
+        uint256 subscriptionId;
+    }
+
+    mapping(uint256 => NetworkConfig) private networkConfigs;
+
+    constructor() {
+        if (block.chainid == ANVIL_CHAIN_ID) {
+            networkConfigs[block.chainid] = getAnvilConfig();
+        } else if (block.chainid == SEPOLIA_CHAIN_ID) {
+            networkConfigs[block.chainid] = getSepoliaConfig();
+        }
+    }
+
+    function getNetworkConfig() public view returns (NetworkConfig memory) {
+        return networkConfigs[block.chainid];
+    }
+
+    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                subscriptionId: 0
+            });
+    }
+
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        // deploy mock vrf coordinator
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(
+            BASE_FEE,
+            GAS_PRICE,
+            WEI_PER_UNIT_LINK
+        );
+        vm.stopBroadcast();
+
+        return
+            NetworkConfig({
+                vrfCoordinator: address(vrfCoordinator),
+                // doesnt matter
+                keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                subscriptionId: 0
+            });
+    }
+}
