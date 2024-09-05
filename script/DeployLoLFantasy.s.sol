48990f13dfc8ee15422836c51b07169c50949109
MichaelSukkiSong <sa77irak@gmail.com>
2024-09-06 07:42:28 +0900

    add: subscription logic in deploy script

--- script/DeployLoLFantasy.s.sol
+++ script/DeployLoLFantasy.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.26;
 import {Script} from "forge-std/Script.sol";
 import {LoLFantasy} from "../src/LoLFantasy.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployLoLFantasy is Script {
     LoLFantasy lolFantasy;
@@ -18,13 +19,35 @@ contract DeployLoLFantasy is Script {
         HelperConfig.NetworkConfig memory networkConfig = helperConfig
             .getNetworkConfig();
 
-        address vrfCoordinator = networkConfig.vrfCoordinator;
-        bytes32 keyHash = networkConfig.keyHash;
-        uint256 subscriptionId = networkConfig.subscriptionId;
+        if (networkConfig.subscriptionId == 0) {
+            // create subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            networkConfig.subscriptionId = createSubscription
+                .createSubscription(networkConfig.vrfCoordinator);
+            // fund subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                networkConfig.vrfCoordinator,
+                networkConfig.subscriptionId
+            );
+        }
 
+        // deploy contract
         vm.startBroadcast();
-        lolFantasy = new LoLFantasy(vrfCoordinator, keyHash, subscriptionId);
+        lolFantasy = new LoLFantasy(
+            networkConfig.vrfCoordinator,
+            networkConfig.keyHash,
+            networkConfig.subscriptionId
+        );
         vm.stopBroadcast();
+
+        // add contract as consumer
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            networkConfig.vrfCoordinator,
+            networkConfig.subscriptionId,
+            address(lolFantasy)
+        );
 
         return lolFantasy;
     }
