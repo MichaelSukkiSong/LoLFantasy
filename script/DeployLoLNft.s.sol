d75f45cfd10303c10cf81c6ff694565e419b2c00
MichaelSukkiSong <sa77irak@gmail.com>
2024-09-19 14:21:27 +0900

    update: lolnft deploy script to have deployer

--- script/DeployLoLNft.s.sol
+++ script/DeployLoLNft.s.sol
@@ -3,12 +3,17 @@
 pragma solidity ^0.8.26;
 import {Script} from "forge-std/Script.sol";
 import {LoLNft} from "../src/LoLNft.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployLoLNft is Script {
     LoLNft public loLNft;
 
     function run() public returns (LoLNft) {
-        vm.startBroadcast();
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig
+            .getNetworkConfig();
+
+        vm.startBroadcast(networkConfig.account);
         loLNft = new LoLNft();
         vm.stopBroadcast();
 
